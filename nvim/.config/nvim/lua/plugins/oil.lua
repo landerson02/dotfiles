3fefae5dfd757e54de29fbb3f306c1e40568177f
Lucas Anderson <lucas.a@comcast.net>
2025-01-08 22:35:58 -0600

    Update oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,13 +1,35 @@
 return {
   'stevearc/oil.nvim',
   config = function()
-    local oil = require 'oil'
-    oil.setup {
+    local oil = require('oil')
+    oil.setup({
       keymaps = {
         ['l'] = 'actions.select',
         ['h'] = 'actions.parent',
+        ['<C-v>'] = { 'actions.select', opts = { vertical = true } },
       },
-    }
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name, bufnr)
+          local ignored = {
+            node_modules = true,
+            ['.'] = true,
+            ['..'] = true,
+            ['.git'] = true,
+            ['.DS_Store'] = true,
+          }
+          return ignored[name] == true
+        end,
+      },
+      float = {
+        -- Padding around the floating window
+        padding = 20,
+        -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        max_width = 0.6,
+        max_height = 0.6,
+        border = 'rounded',
+      },
+    })
     vim.keymap.set('n', '<leader>o', oil.toggle_float, { desc = '[O]il' })
 
     -- Close on q
